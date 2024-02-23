7644b94192e1d692bf0a3409a1b9d5fcca4c6d1b
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-23 22:24:21 +0400

    definitely not solid

---
+++ Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    internal class Item
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    internal class Order
+    {
+        public byte Status { get; set; }
+        public List<Item> Items { get; set; } = [];
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+        public decimal OrderPrice()
+        {
+            return Items.Aggregate(0M, ( sum, item) => sum + item.Price);
+        }
+
+        public void Pay(string paymenttype, string securityCode)
+        {
+            if (paymenttype == "cash")
+            {
+                Console.WriteLine($"Processing -cash- payment for {OrderPrice()} with security code {securityCode}");
+            }
+            else if (paymenttype == "card")
+            {
+                Console.WriteLine($"Processing -card- payment for {OrderPrice()} with security code {securityCode}");
+            }
+            else
+            {
+                throw new ArgumentException($"The -{paymenttype}- type of payment is not availible");
+            }
+        }
+    }
+}
