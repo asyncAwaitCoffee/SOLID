90d10a32e75d5250a30dddfb0a62a0feba915359
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-23 22:32:17 +0400

    +cw | still not solid

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Item paper = new Item() { Name = "Paper", Price = 1M, Quantity = 50 };
+            Item pencil = new Item() { Name = "Pencil", Price = 5.25M, Quantity = 4 };
+            Item eraser = new Item() { Name = "Eraser", Price = 1.99M, Quantity = 1 };
+
+            Order order = new Order();
+
+            order.AddItem(paper);
+            order.AddItem(pencil);
+            order.AddItem(eraser);
+
+            order.Pay("card", "A123");
+            order.Pay("cash", "A123");
+            //order.Pay("magic beans", "A123");
         }
     }
 }
